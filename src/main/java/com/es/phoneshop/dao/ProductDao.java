f893e23f849036620b84542796c55b7768c8c88c
L <ap.qwert@yandex.ru>
2020-01-30 18:54:23 +0300

    minor improvements

--- src/main/java/com/es/phoneshop/dao/ProductDao.java
+++ src/main/java/com/es/phoneshop/dao/ProductDao.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface ProductDao {
     Product getProduct(Long id);
+
     List<Product> findProducts();
+
     void save(Product product);
+
     void delete(Long id);
 }
