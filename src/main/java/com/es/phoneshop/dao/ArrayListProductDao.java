bb056eda5965d07292449ed44beae9d7ee1f372b
L <ap.qwert@yandex.ru>
2020-01-30 18:53:54 +0300

    first implementation of ArrayListProductDao

--- src/main/java/com/es/phoneshop/dao/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/dao/ArrayListProductDao.java
@@ -1,27 +1,50 @@
 package com.es.phoneshop.dao;
 
+import com.es.phoneshop.exception.ProductNotFoundException;
 import com.es.phoneshop.model.product.Product;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+
+    public static final int EMPTY_STOCK_LEVEL = 0;
+
+    private List<Product> listOfProducts;
+
+    public ArrayListProductDao() {
+        listOfProducts = new CopyOnWriteArrayList<>();
+    }
+
     @Override
     public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+        return listOfProducts.stream()
+                .filter(product -> Objects.nonNull(product.getPrice()))
+                .filter(product -> product.getStock() > EMPTY_STOCK_LEVEL)
+                .filter(product -> product.getId().equals(id))
+                .findFirst()
+                .orElseThrow(
+                        () -> new ProductNotFoundException(String.format("Product with id %s not found!", id))
+                );
     }
 
     @Override
     public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+        return listOfProducts.stream()
+                .filter(product -> Objects.nonNull(product.getPrice()))
+                .filter(product -> product.getStock() > EMPTY_STOCK_LEVEL)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+        listOfProducts.add(product);
     }
 
     @Override
     public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+        listOfProducts.removeIf(product -> product.getId().equals(id));
     }
 }
