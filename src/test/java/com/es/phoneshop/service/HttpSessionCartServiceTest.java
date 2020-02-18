a1ceba5e0af24e19c309066689ce38261fdd1dab
L <ap.qwert@yandex.ru>
2020-02-18 15:11:52 +0300

    fix: added tests

---
+++ src/test/java/com/es/phoneshop/service/HttpSessionCartServiceTest.java
@@ -0,0 +1,16 @@
+package com.es.phoneshop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HttpSessionCartServiceTest {
+
+    @Test
+    public void getCart() {
+    }
+
+    @Test
+    public void add() {
+    }
+}
